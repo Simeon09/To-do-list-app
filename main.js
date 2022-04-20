84025bf6f03ea186b4fb20baf971f6a6b9dc630a
unknown <simeonolawale69@gmail.com>
2022-04-20 14:12:14 +0100

    solve the puzzle behind d javascript

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const todoitems = document.querySelector("#task")[0];
+const todoitems = document.querySelector("#task");
 const inputs = document.querySelector(".input");
 const form = document.querySelector(".form");
 const edit = document.querySelector(".edit");
@@ -13,25 +13,26 @@ form.addEventListener("submit", (event) => {
     return;
   }
 
-  var parentdiv = document.createElement("div");
-  parentdiv.classList.add("to-doItems");
-  todoitems.appendChild("childdiv");
-  var childdiv = document.createElement ('div');
-  childdiv.classList.add('.Items');
-  childdiv.appendChild('parentdiv');
-  parentdiv.innerText = todoitems;
+  const parentDiv = document.createElement('div');
+  parentDiv.classList.add("Items");
+  // todoitems.appendChild("childdiv");
+  const childDiv = document.createElement ('div');
+  childDiv.classList.add('to-doItems');
+  childDiv.innerText = inputvalue;
+  parentDiv.appendChild(childDiv);
+  todoitems.appendChild(parentDiv);
   // else {
   //     additems();
   // }
 });
-function additems() {
-  var parentdiv = document.createElement("div");
-  parentdiv.classList.add("to-doItems");
-  todoitems.appendChild("parentdiv");
-  parentdiv.innerText = todoitems;
-  var editbutton = document.createElement("button");
-  var trashbutton = document.createElement("button");
+// function additems() {
+//   var parentdiv = document.createElement("div");
+//   parentdiv.classList.add("to-doItems");
+//   todoitems.appendChild("parentdiv");
+//   parentdiv.innerText = todoitems;
+//   var editbutton = document.createElement("button");
+//   var trashbutton = document.createElement("button");
 
-  // parentdiv.className='to-doItems';
-  // parentdiv.innerHTML = 'div' + inputs.value + 'div';
-}
+//   // parentdiv.className='to-doItems';
+//   // parentdiv.innerHTML = 'div' + inputs.value + 'div';
+// }
